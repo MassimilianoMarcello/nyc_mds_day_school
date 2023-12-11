3dc1cffd59d7e3dd88fade0c4004a872367ffb9e
massimiliano <massimarcello@hotmail.it>
2023-12-11 03:21:22 +0100

    refixing mobile

--- src/components/Main Content/SixSection.js
+++ src/components/Main Content/SixSection.js
@@ -3,17 +3,17 @@ import "./SixSection..css";
 
 const SixSection = () => {
   return (
-    <section className="section6 parallax-d">
-      <div className="para-container-d">
-        <h1 className="title-d">Join our family</h1>
+    <section className="section6 parallax-6">
+      <div className="para-container-6">
+        <h1 className="title-6">Join our family</h1>
 
-        <p className="text-d">
+        <p className="text-6">
           The future is bright: feel the warmth. <br /> Join the MDS family today to
           experience the magic of MDS firsthand. <br /> 
         </p>
-        <div className="ask-link-container">
-          <p className="text-d">Have a question about application?</p>
-          <a href="./" className="ask-link">Ask Here</a>
+        <div className="ask-link-container6">
+          <p className="text-6">Have a question about application?</p>
+          <a href="./" className="ask-link6">Ask Here</a>
         </div>
         <a href="./" className="button-6">apply now</a>
       </div>
