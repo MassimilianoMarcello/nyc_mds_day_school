93cb6bde79ddc297b3a94f1cf5a9c57b7e873b96
massimiliano <massimarcello@hotmail.it>
2023-12-12 23:07:05 +0100

    made last time sector 3  menu does not wor a certain resolution

--- src/components/Main Content/ThreeSection.js
+++ src/components/Main Content/ThreeSection.js
@@ -6,31 +6,33 @@ const ThreeSection = () => {
     <>
       <section className="parallax9">
         <ul className="para-container">
-          <li className="a1">
-            <img src="/assets/hero_image2.jpg" alt="teacher and boy reading" />
+          <li className="a-image-container">
+            <img
+              src="/assets/hero_image2.jpg"
+              alt="teacher and boy reading"
+              className="a1"
+            />
+
+           <h1 className="a5 "><span className="rectangle1"></span></h1> 
           </li>
+
           <li className="a-text-container">
-            <li className="a2">
-              <h1 className="title3-tex">
-                Early Childhood Education
-                <span className="text-gold">
-                  designed to nurture and engage young minds
-                </span>
-              </h1>
-            </li>
-            <li className="a3">
-              <p className="a3">
-                MDS creates a warm and welcoming place for our youngest learners
-                -toddlers through kindergartners- to drive as they begin their
-                educational journey.
-              </p>
-            </li>
-            <li className="a4">
-              <a href="./">learn more</a>
-            </li>
-          </li>
-          <li className="a5">
-            <span className="rectangle1"></span>
+            <h1 className="a2 title3-tex">
+              Early Childhood Education
+              <span className="text-gold">
+                designed to nurture and engage young minds
+              </span>
+            </h1>
+
+            <p className="a3">
+              MDS creates a warm and welcoming place for our youngest learners
+              -toddlers through kindergartners- to drive as they begin their
+              educational journey.
+            </p>
+
+           <h1 className="a4"><a href="./" >
+              learn more  </a></h1> 
+          
           </li>
         </ul>
       </section>
