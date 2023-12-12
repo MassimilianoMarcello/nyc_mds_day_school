ca5d5255894853b6c49f253d801a13b79dd3a3de
massimiliano <massimarcello@hotmail.it>
2023-12-13 00:43:37 +0100

    started section 4 and 5  sos saving

--- src/components/Main Content/FiveSection.js
+++ src/components/Main Content/FiveSection.js
@@ -1,39 +1,40 @@
-import React from 'react'
-import "./FiveSection.css"
+import React from "react";
+import "./FiveSection.css";
 
 const FiveSection = () => {
   return (
+    <>
+      <section className="parallax-c">
+        <ul className="para-container-c">
+          <li className="c-image-container">
+            <img src="/assets/hero image4.png" alt="young scientist" />
 
-  <section className="parallax-c">
-  <ul className="para-container-c">
-    <li className="c1">
-    <img src="/assets/hero image4.png" alt="young scientist" />
-    </li>
-    <li className='c-text-container'>
-    <li className="c2">
-    <h1>
-      <span className="gold-text">Middle School  </span> helps groving student fluorish
-    </h1>
-    </li>
-    <li className="c3">
-    <p>
-      When entering Middle School, students dive deeper into academic
-      subjects, critical thinking and questioning, and logical reasoning.
-    </p>
-    </li>
-    <li className="c4">
-      
-      <a href="./">learn more</a>
-    </li>
-    </li>
-    
-    {/* <li className="c5">
-      
-      <span className="rectangle1"></span>
-    </li> */}
-  </ul>
-</section>
-  )
-}
+            <h1 className="c5 ">
+              <span className="rectangle1"></span>
+            </h1>
+          </li>
 
-export default FiveSection
+          <li className="a-text-container">
+            <h1 className="c2 title3-tex">
+              Middle School
+              <span className="text-gold">helps groving student fluorish</span>
+            </h1>
+
+            <p className="c3">
+              When entering Middle School, students dive deeper into academic
+              subjects, critical thinking and questioning, and logical
+              reasoning.
+            </p>
+
+            <h1 className="c4">
+              <a href="./">learn more </a>
+            </h1>
+          </li>
+        </ul>
+      </section>
+    </>
+
+  );
+};
+
+export default FiveSection;
--- src/components/Main Content/FourSection.js
+++ src/components/Main Content/FourSection.js
@@ -3,35 +3,40 @@ import "./FourSection.css";
 
 const FourSection = () => {
   return (
-    <section className="parallax-b">
-      <ul className="para-container-b">
-        <li className="b1">
-          <img src="/assets/hero_image3.png" alt="teacher and boy reading" />
-        </li>
-        <li className="b-text-container">
-          
-          <li className="b2">
-            <h1 className="title3-tex">
+    <>
+      <section className="parallax-b">
+        <ul className="para-container-b">
+          <li className="b-image-container">
+            <img
+              src="/assets/hero_image3.png"
+              alt="children smiling"
+              className="b1"
+            />
+
+            <h1 className="b5 ">
+              <span className="rectangle1"></span>
+            </h1>
+          </li>
+
+          <li className="b-text-container">
+            <h1 className="b2 title3-tex">
               <span className="text-gold">Lower School </span> builds a solid
               foundation for life
             </h1>
-          </li>
-          <li className="b3">
+
             <p className="b3">
               Students explore their studies through unique, interdisciplinary
               hands-on experiences and state-of-the-art technology and begin to
               develop core academic skills.
             </p>
+
+            <h1 className="b4">
+              <a href="/sign-up">learn more </a>
+            </h1>
           </li>
-          <li className="b4">
-            <a href="./">learn more</a>
-          </li>
-        </li>
-        <li className="b5">
-          <span className="rectangle1"></span>
-        </li>
-      </ul>
-    </section>
+        </ul>
+      </section>
+    </>
   );
 };
 
